9bb78c245cae06aabdfd9e8030e0af654ecca156
mukucodes <150809693+mukucodes@users.noreply.github.com>
2023-12-02 15:29:06 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.mukucodes.com/', // Write here your website url
+	site: 'https://mukucodes.com/', // Write here your website url
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
